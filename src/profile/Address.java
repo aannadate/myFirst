dccce3b92125d3ddda3b9536dadd5d08024c78bd
aniket <annadateaniket@gmail.com>
2023-03-22 16:04:59 +0530

    first git commit

---
+++ src/profile/Address.java
@@ -0,0 +1,13 @@
+package profile;
+
+public class Address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void address() {
+		System.out.println("Address");
+	}
+
+}
---
+++ src/profile/Details.java
@@ -0,0 +1,10 @@
+package profile;
+
+public class Details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Details ");
+	}
+
+}
